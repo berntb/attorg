efe88a23c166ccc2225e9dbfb473a903d149271c
Bernt Budde <bernt.budde@gmail.com>
2014-07-23 13:31:13 +0300

    Forgot license on a file

--- public/javascripts/attorg_cmd_list.js
+++ public/javascripts/attorg_cmd_list.js
@@ -1,3 +1,20 @@
+//    This file is part of attorg.  Copyright 2013, 2014 Bernt Budde.
+//
+//    Attorg is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    Attorg is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with Attorg.  If not, see <http://www.gnu.org/licenses/>.
+
+//    The author is Bernt Budde, see my GitHub account, user berntb.
+// ----------------------------------------------------------------------
 
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
